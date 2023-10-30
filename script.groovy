aa0a77d4ff7cc477b35cb2a105413c34d77df18a
phpnoida <amitrepublik@gmail.com>
2023-10-30 07:07:05 -0400

    checking again working or not

--- script.groovy
+++ script.groovy
@@ -1,9 +1,9 @@
 def buildImage(){
     echo "building dev api docker image..."
     withCredentials([usernamePassword(credentialsId:'dockerhub-cred',passwordVariable:'PASS',usernameVariable:'USER')]){
-        sh "docker build -t incredgolf/golf:$IMAGE_NAME ."
+        sh "docker build -t incredgolf/golf:${IMAGE_NAME} ."
         sh 'echo $PASS |docker login -u $USER --password-stdin'
-        sh "docker push incredgolf/golf:$IMAGE_NAME"
+        sh "docker push incredgolf/golf:${IMAGE_NAME}"
     }
     
 }

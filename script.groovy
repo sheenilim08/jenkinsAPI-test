48e077143be32114365af7975f320fe9e0c95cc4
sheenilim08 <sheenismhael.lim@outlook.ph>
2023-11-01 10:57:05 +0800

    updated docker hub creds

--- script.groovy
+++ script.groovy
@@ -1,32 +1,31 @@
-def buildImage(){
-    echo "building dev api docker image..."
-    withCredentials([usernamePassword(credentialsId:'dockerhub-cred',passwordVariable:'PASS',usernameVariable:'USER')]){
+def buildImage() {
+    echo 'building dev api docker image...'
+    withCredentials([usernamePassword(credentialsId:'docker-hub-credentials.', passwordVariable:'PASS', usernameVariable:'USER')]) {
         sh "docker build -t incredgolf/golf:${IMAGE_NAME} ."
         sh 'echo $PASS |docker login -u $USER --password-stdin'
         sh "docker push incredgolf/golf:${IMAGE_NAME}"
     }
-    
 }
 
-def deployAPI(){
-    echo "deploying dev api..."
-    echo "deploying dev......"
+def deployAPI() {
+    echo 'deploying dev api...'
+    echo 'deploying dev......'
 }
 
-def testAPI(){
-    echo "testing dev api..."
+def testAPI() {
+    echo 'testing dev api...'
 }
 
-def commitBackToGit(){
-    echo "updating package.json in git..."
-   withCredentials([usernamePassword(credentialsId:'github-creds',passwordVariable:'PASS',usernameVariable:'USER')]){
+def commitBackToGit() {
+    echo 'updating package.json in git...'
+    sshagent(credentials: ['gitlab-ssh']) {
         sh 'git status'
         sh 'git branch'
         sh 'git config --list'
-        sh "git remote set-url origin https://${USER}:${PASS}@github.com/IncredApplicationsPvtLtd/jenkinsAPI.git"
-        sh "git add ."
+        sh 'git remote set-url origin git@github.com:sheenilim08/jenkinsAPI-test.git'
+        sh 'git add .'
         sh "git commit -m 'ci:version bump'"
-        sh "git push origin HEAD:main"
+        sh 'git push origin HEAD:main'
     }
 }
 

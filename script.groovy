75ccd73c65e0786b93a888d7a8bf58dfd76132c9
sheenilim08 <sheenismhael.lim@outlook.ph>
2023-11-01 11:05:02 +0800

    Changed docker hub repo image name

--- script.groovy
+++ script.groovy
@@ -1,9 +1,9 @@
 def buildImage() {
     echo 'building dev api docker image...'
     withCredentials([usernamePassword(credentialsId:'docker-hub-credentials.', passwordVariable:'PASS', usernameVariable:'USER')]) {
-        sh "docker build -t incredgolf/golf:${IMAGE_NAME} ."
+        sh "docker build -t sheenismhaellim/itfellas:${IMAGE_NAME} ."
         sh 'echo $PASS |docker login -u $USER --password-stdin'
-        sh "docker push incredgolf/golf:${IMAGE_NAME}"
+        sh "docker push sheenismhaellim/itfellas:${IMAGE_NAME}"
     }
 }
 
